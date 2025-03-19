1e3a6453fbefe2db78b93ca506f5ff14d33eafb1
Nats306 <nati30062006@gmail.com>
2025-03-19 10:03:27 -0500

    correccion

--- routes/libroRoutes.js
+++ routes/libroRoutes.js
@@ -5,6 +5,6 @@ const libroController = require('../controller/libroController');
 router.post('/', libroController.addLibro);
 router.get('/', libroController.getLibros);
 router.put('/:id', libroController.updateLibro);
-router.put('/:id', libroController.changeStatus);
+router.post("/ChangeStatus/:id", libroController.changeStatus);
 
 module.exports = router;
